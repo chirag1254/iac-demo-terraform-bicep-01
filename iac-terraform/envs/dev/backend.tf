5a28fcc420bc3151c5c0b101791dbed8a7964c1b
Chirag <chiragsachdeva1254@gmail.com>
2025-09-28 13:26:23 +0530

    backend

---
+++ iac-terraform/envs/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "iac-tfstate-rg"
+    storage_account_name = "iacstatestorage01 "
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
